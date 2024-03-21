fbad9b776f17378104c1bb78a6193babfc3c0c05
Augusto Silva <augusto.a.silva@hotmail.com>
2024-03-21 19:17:38 -0300

    fix: corrige migration que nao criava matricula unica

--- src/main/resources/db/migration/V1__Create_Employee_Table.sql
+++ src/main/resources/db/migration/V1__Create_Employee_Table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE employees
 (
-    id         SERIAL PRIMARY KEY NOT NULL,
-    registry   VARCHAR(255)       NOT NULL,
-    password   VARCHAR(255)       NOT NULL,
-    role       VARCHAR(255)       NOT NULL,
-    created_at TIMESTAMP          NOT NULL,
-    updated_at TIMESTAMP          NOT NULL
+    id         SERIAL PRIMARY KEY  NOT NULL,
+    registry   VARCHAR(255) UNIQUE NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    role       VARCHAR(255)        NOT NULL,
+    created_at TIMESTAMP           NOT NULL,
+    updated_at TIMESTAMP           NOT NULL
 );
